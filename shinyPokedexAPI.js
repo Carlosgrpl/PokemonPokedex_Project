7617b9dba7237d690b5e21356d6e896f392c2a9d
Carlosgrpl <cgonzalez.38@alumni.unav.es>
2024-05-11 17:13:27 +0200

    Shiny pokedex progress, we're getting there, step by step

--- shinyPokedexAPI.js
+++ shinyPokedexAPI.js
@@ -3,8 +3,20 @@ let pokedex =
   {}; /* STRUCTURE {1 : {"name": "bulbasaur", "img": url, "type": ["grass", "poison"], "description": "..."}} */
 
 window.onload = async function () {
-  getPokemon(1);
-}; /* Onload is an event used to automatically display the 1st pkmn once the page is loaded */
+  /* getPokemon(1); */
+  for (let i = 1; i <= pokemonCount; i++) {
+    await getPokemon(i);
+    /* <div> id="1" class="pokemonName"> BULBASAUR </div>  */
+    let pokemon = document.createElement("div");
+    pokemon.id = i; /* Whe set the id so that then we can use onclick */
+    ppokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
+    pokemon.classList.add("pokemonName");
+    pokemon.addEventListener("click", UpdatePokemon);
+    document.getElementById("pkmnList").append(pokemon);
+  }
+  /* Onload is an event used to automatically display the 1st pkmn once the page is loaded */
+  console.log(pokedex);
+};
 /* Fetch always go wit async and await, it NEEDS an async function to work properly */
 async function getPokemon(num) {
   let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
@@ -13,8 +25,27 @@ async function getPokemon(num) {
   /* Every time you have await in a function you need to use an async function */
   let pokemon = await res.json();
   /* await: Waits for the promise to be fulfilled */ console.log(pokemon);
+
+  let pokemonName = pokemon["name"];
+  let pokemonType = pokemon["types"]
+    .map((type) => type["type"]["name"])
+    .join(", "); // Convert types to string
+  let pokemonImage = pokemon["sprites"]["front_shiny"];
+
+  /* 
+console.log(pokemonDescription); */
+  let speciesRes = await fetch(pokemon["species"]["url"]);
+  let speciesData = await speciesRes.json();
+  let pokemonDescription = speciesData["flavor_text_entries"][2]["flavor_text"];
+
+  pokedex[num] = {
+    name: pokemonName,
+    img: pokemonImage,
+    type: pokemonType,
+    description: pokemonDescription,
+  };
 }
 
-let pokemonName = pokemon["name"];
-let pokemonType = pokemon["types"];
-let pokemonImage = pokemon["sprites"]["front_shiny"];
+function UpdatePokemon() {
+  document.getElementById("pkmnImage").src = pokedex[this.id]["img"];
+}
